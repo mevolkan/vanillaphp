2364f53bf0a71a1cd63288539135aeccbbe207b6
volkan <nzaro19@gmail.com>
2023-11-15 20:52:35 +0300

    patient form working

--- includes/footer.php
+++ includes/footer.php
@@ -148,19 +148,19 @@
 				</button>
 			</div>
 			<!-- Modal body -->
-			<form action="#" class="p-4 md:p-5">
+			<form id="patient-form" action="#" class="p-4 md:p-5">
 				<div class="grid grid-cols-2 gap-4 mb-4">
 					<div class="col-span-2">
-						<label for="firstname"
+						<label for="firstName"
 							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-						<input type="text" name="firstname" id="firstname"
+						<input type="text" name="firstName" id="firstName"
 							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 							placeholder="First Name" required="">
 					</div>
 					<div class="col-span-2">
-						<label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
+						<label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
 							Name</label>
-						<input type="text" name="lastname" id="lastname"
+						<input type="text" name="lastName" id="lastName"
 							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 							placeholder="Last Name" required="">
 					</div>
@@ -174,24 +174,24 @@
 					<div class="col-span-2 sm:col-span-1">
 						<label for="gender"
 							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
-						<select id="gender"
+						<select name="gender" id="gender"
 							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-							<option selected="">Select Gender</option>
-							<option value="TV">Male</option>
-							<option value="PC">Female</option>
+							<option value="Male">Male</option>
+							<option value="Female">Female</option>
 						</select>
 					</div>
 				</div>
 				<div class="col-span-2">
-				<button type="reset"
-					class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Clear
-				</button>
+					<button type="reset"
+						class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						Clear
+					</button>
 
-				<button type="submit"
-					class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Save
-				</button>
+					<button type="button" onclick="submitForm()"
+						class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						Save
+					</button>
+					<div id="status"></div>
 				</div>
 			</form>
 		</div>
@@ -199,7 +199,44 @@
 </div>
 
 
+<script>
+	function submitForm() {
+		const form = document.getElementById('patient-form');
+		const formData = new FormData(form);
+		const jsonData = {};
+
+		formData.forEach((value, key) => {
+			jsonData[key] = value;
+		});
+		console.log(formData)
+		const apiUrl = 'http://localhost:3000/patients';
+
+		fetch(apiUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(jsonData),
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then(data => {
+				// Handle the successful response
+				document.getElementById('status').innerText = JSON.stringify(data, null, 2);
+			})
+			.catch(error => {
+				// Handle errors
+				console.error('Error:', error);
+			});
+	}
+</script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js"></script>
+
 
 </body>
 
---
+++ includes/js/scripts.js
@@ -0,0 +1,15 @@
+// document
+//     .getElementById("patient-form")
+//     .addEventListener("submit", function (event) {
+//         event.preventDefault();
+
+//         var formData = new FormData(this);
+//         var xhr = new XMLHttpRequest();
+//         xhr.open("POST", "localhost:3000", true);
+//         xhr.onload = function () {
+//             if (xhr.status === 200) {
+//                 console.log(xhr.responseText);
+//             }
+//         };
+//         xhr.send(formData);
+//     });
