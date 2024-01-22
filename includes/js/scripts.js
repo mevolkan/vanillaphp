1f7e8b6115b38f64bb91ac201b8ed0f73a7ab1c7
volkan <nzaro19@gmail.com>
2024-01-22 22:01:47 +0300

    script formating

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -28,14 +28,13 @@ function calculateBMI() {
     bmiField.placeholder = bmi;
     if (bmi > 25) {
       setFieldVisibility(false);
-      
     } else {
       setFieldVisibility(true);
     }
   }
 }
 
-// submit forms
+// submit patient form
 function submitPatientForm() {
   const form = document.getElementById("patient-form");
   const formData = new FormData(form);
@@ -61,7 +60,7 @@ function submitPatientForm() {
       return response.json();
     })
     .then((data) => {
-      document.getElementById("status").innerText = JSON.stringify(
+      document.getElementById("patient-form").innerText = JSON.stringify(
         data,
         null,
         2
@@ -72,39 +71,45 @@ function submitPatientForm() {
     });
 }
 
+//submit vital form
 function submitVitalForm() {
-    const form = document.getElementById("vital-form");
-    const formData = new FormData(form);
-    const jsonData = {};
-  
-    formData.forEach((value, key) => {
-      jsonData[key] = value;
-    });
-  
-    const apiUrl = "http://localhost:3000/vitals";
-  
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(jsonData),
+  const form = document.getElementById("vital-form");
+  const formData = new FormData(form);
+  const jsonData = {};
+
+  formData.forEach((value, key) => {
+    jsonData[key] = value;
+  });
+
+  const apiUrl = "http://localhost:3000/vitals";
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        document.getElementById("status").innerText = JSON.stringify(
-          data,
-          null,
-          2
-        );
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
-  
+    .then((data) => {
+      document.getElementById("vital-form").innerText = JSON.stringify(
+        data,
+        null,
+        2
+      );
+      document.getElementById("vital-form").reset();
+      window.setTimeout(function () {
+        document.getElementById("vital-form").reset();
+        location.reload();
+
+      }, 3000);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
